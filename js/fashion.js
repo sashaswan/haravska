a789bacc21442cfc86aefbba0e9af4c2a5f220ae
Sasha Swan <alexandrio748@gmail.com>
2025-03-19 17:05:09 +0200

    Update fashion.js

--- js/fashion.js
+++ js/fashion.js
@@ -1,93 +1,93 @@
 var data = [
 	{
 		type: 'left',
-		image: '/images/fashion/1.png',
+		image: 'https://sashaswan.github.io/haravska/images/fashion/1.png',
 		title: 'GREEN LINE',
 		model: 'Viktoria Rakocha',
 		modelAgency: '/ Grace Models',
 		modelName: 'Sofia Slobodian',
-		style:'Nataliya Novitska',
-		slider: '/images/fashion/green/1.jpg',
-		slider1: '/images/fashion/green/2.jpg',
-		slider2: '/images/fashion/green/3.jpg',
-		link:'/greenline',
+		style: 'Nataliya Novitska',
+		slider: 'https://sashaswan.github.io/haravska/images/fashion/green/1.jpg',
+		slider1: 'https://sashaswan.github.io/haravska/images/fashion/green/2.jpg',
+		slider2: 'https://sashaswan.github.io/haravska/images/fashion/green/3.jpg',
+		link: '/greenline',
 		mua: 'Mua:',
 		styleMark: 'Style:'
 	},
 	{
 		type: 'right',
-		image: '/images/fashion/2.jpg',
+		image: 'https://sashaswan.github.io/haravska/images/fashion/2.jpg',
 		title: 'IN BLOOM',
 		model: 'Katerina Gyulmamedova',
 		modelAgency: '',
 		modelName: 'Margo Parashchenko',
-		style:'Roman Timofeev',
+		style: 'Roman Timofeev',
 		designer: 'Rito',
-		slider: '/images/fashion/bloom/1.jpg',
-		slider1: '/images/fashion/bloom/2.jpg',
-		slider2: '/images/fashion/bloom/3.jpg',
-		link:'/inbloom',
+		slider: 'https://sashaswan.github.io/haravska/images/fashion/bloom/1.jpg',
+		slider1: 'https://sashaswan.github.io/haravska/images/fashion/bloom/2.jpg',
+		slider2: 'https://sashaswan.github.io/haravska/images/fashion/bloom/3.jpg',
+		link: '/inbloom',
 		mua: 'Mua:',
 		styleMark: 'Style:'
 	},
 	{
 		type: 'left',
-		image: '/images/fashion/3.png',
+		image: 'https://sashaswan.github.io/haravska/images/fashion/3.png',
 		title: 'IN THE MIDDLE OF NOWHERE',
 		model: 'Maria Valyukh',
 		modelAgency: '/ OK’S Models',
 		modelName: '',
 		style: 'Natasha Kuzmych',
-		slider: '/images/fashion/inthe/1.jpg',
-		slider1: '/images/fashion/inthe/2.jpg',
-		slider2: '/images/fashion/inthe/3.jpg',
-		link:'/nowhere',
+		slider: 'https://sashaswan.github.io/haravska/images/fashion/inthe/1.jpg',
+		slider1: 'https://sashaswan.github.io/haravska/images/fashion/inthe/2.jpg',
+		slider2: 'https://sashaswan.github.io/haravska/images/fashion/inthe/3.jpg',
+		link: '/nowhere',
 		mua: '',
 		styleMark: 'Style:'
 	},
 	{
 		type: 'right',
-		image: '/images/fashion/4.png',
-		title: 'RUSH',	
+		image: 'https://sashaswan.github.io/haravska/images/fashion/4.png',
+		title: 'RUSH',
 		model: 'Lilia Datsyk',
 		modelAgency: '',
 		modelName: '',
 		style: '',
-		slider: '/images/fashion/rush/1.jpg',
-		slider1: '/images/fashion/rush/2.jpg',
-		slider2: '/images/fashion/rush/3.jpg',
-		link:'/rush',
+		slider: 'https://sashaswan.github.io/haravska/images/fashion/rush/1.jpg',
+		slider1: 'https://sashaswan.github.io/haravska/images/fashion/rush/2.jpg',
+		slider2: 'https://sashaswan.github.io/haravska/images/fashion/rush/3.jpg',
+		link: '/rush',
 		mua: '',
 		styleMark: ''
 	},
 
 	{
 		type: 'left',
-		image: '/images/fashion/5.png',
+		image: 'https://sashaswan.github.io/haravska/images/fashion/5.png',
 		title: 'WANDERLUST',
 		model: 'Lidiya Lisova',
 		modelAgency: '/ Grace Models',
 		modelName: '',
 		style: '',
-		slider: '/images/fashion/lust/1.jpg',
-		slider1: '/images/fashion/lust/2.jpg',
-		slider2: '/images/fashion/lust/3.jpg',
-		link:'/wanderlust',
+		slider: 'https://sashaswan.github.io/haravska/images/fashion/lust/1.jpg',
+		slider1: 'https://sashaswan.github.io/haravska/images/fashion/lust/2.jpg',
+		slider2: 'https://sashaswan.github.io/haravska/images/fashion/lust/3.jpg',
+		link: '/wanderlust',
 		mua: '',
 		styleMark: ''
 	},
 	{
 		type: 'right',
-		image: '/images/yellowglow/slide1.jpg',
+		image: 'https://sashaswan.github.io/haravska/images/yellowglow/slide1.jpg',
 		title: 'YELLOW GLOW',
 		model: 'Sofi Krishchuk',
 		modelAgency: '/ OK’S Models',
 		modelName: '',
-		style:'Alina Haravska',
-		slider: '/images/fashion/yellow/1.jpg',
-		slider1: '/images/fashion/yellow/2.jpg',
-		slider2: '/images/fashion/yellow/3.jpg',
-		link:'/yellowglow',
+		style: 'Alina Haravska',
+		slider: 'https://sashaswan.github.io/haravska/images/fashion/yellow/1.jpg',
+		slider1: 'https://sashaswan.github.io/haravska/images/fashion/yellow/2.jpg',
+		slider2: 'https://sashaswan.github.io/haravska/images/fashion/yellow/3.jpg',
+		link: '/yellowglow',
 		mua: '',
 		styleMark: 'Style:'
 	}
@@ -96,18 +96,18 @@ var types = ['left', 'right', 'another'];
 var templates = {};
 var promises = [];
 for (var i = 0; i < types.length; i++) {
-	var promise = new Promise (function (resolve, reject) {
+	var promise = new Promise(function (resolve, reject) {
 		var rawFile = new XMLHttpRequest();
-	    rawFile.open("GET", '/fashiontype/' + types[i] + '.html', false);
-	    rawFile.onreadystatechange = function () {
-	        if(rawFile.readyState === 4) {
-	            if(rawFile.status === 200 || rawFile.status == 0) {
+		rawFile.open("GET", '/fashiontype/' + types[i] + '.html', false);
+		rawFile.onreadystatechange = function () {
+			if (rawFile.readyState === 4) {
+				if (rawFile.status === 200 || rawFile.status == 0) {
 					templates[types[i]] = rawFile.responseText;
 					resolve();
-	            }
-	        }
-	    }
-	    rawFile.send(null);
+				}
+			}
+		}
+		rawFile.send(null);
 	});
 	promises.push(promise);
 }
@@ -137,7 +137,7 @@ function buildHtml() {
 			template = template
 				.replace('{{showdesign}}', '')
 				.replace('{{/showdesign}}', '');
-		}	
+		}
 		$('.fashionpage').append(template);
 	}
 	console.log(templates);
